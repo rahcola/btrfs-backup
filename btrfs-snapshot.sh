48a00c4e0a16aca373cd5f13c11c701e6fffa3be
Jani Rahkola <jani.rahkola@reaktor.fi>
2014-07-31 15:22:51 +0300

    merged snapshoting and pruning

--- btrfs-snapshot.sh
+++ btrfs-snapshot.sh
@@ -2,11 +2,37 @@
 
 set -e
 TO=$(realpath -e "$1")
+NOW=$(date +'%s')
+ISO_NOW=$(date +'%Y-%m-%dT%H:%M:%S')
+PRUNE_LIMIT=$((60 * 60 * 24))
 shift
 
+function prune() {
+    VOL="$1"
+    LATEST_BACKUP=$(realpath -e "${TO}/${VOL}-latest-backup")
+    for S in $(echo "${TO}/${VOL}*"); do
+        SNAPSHOT=$(realpath -e ${S})
+        if [ ${SNAPSHOT} = ${LATEST_BACKUP} ]; then
+            continue
+        fi
+
+        TIME=$(date -d$(echo $(basename ${SNAPSHOT}) | cut -d '-' -f 2-) +'%s')
+        if [ $((${NOW} - ${TIME})) -ge ${PRUNE_LIMIT} ]; then
+            btrfs subvol delete ${SNAPSHOT}
+        fi
+    done
+}
+
+function snapshot() {
+    VOL="$1"
+    VOL_FULL="$2"
+    btrfs subvol snapshot -r "${VOL_FULL}" "${TO}/${VOL}-${ISO_NOW}"
+}
+
 for V in "$@"; do
-    VOL=$(realpath -e ${V})
-    VOLNAME=$(basename ${VOL})
-    btrfs subvol snapshot -r ${VOL} ${TO}/${VOLNAME}-$(date +'%Y-%m-%dT%H:%M:%S')
+    VOL_FULL=$(realpath -e ${V})
+    VOL=$(basename ${VOL_FULL})
+    prune ${VOL}
+    snapshot ${VOL} ${VOL_FULL}
 done
 sync
