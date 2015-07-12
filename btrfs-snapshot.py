a7f462fa523c1c876dd215688e620907031802db
Jani Rahkola <jani.rahkola@iki.fi>
2015-07-12 12:47:29 +0300

    Btrfs code into a module

--- btrfs-snapshot.py
+++ btrfs-snapshot.py
@@ -1,63 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
-import glob
-import os.path
-import re
+import btrfs
 import subprocess
 import time
-
-class Subvolume(object):
-    def __init__(self, path, name, uuid, parent, created):
-        self.path = path
-        self.name = name
-        self.uuid = uuid
-        self.parent = parent
-        self.created = created
-
-    def snapshot(self, destination, name):
-        if os.path.isabs(destination):
-            snapshot = os.path.join(destination, name)
-        else:
-            snapshot = os.path.join(self.path, destination, name)
-        subprocess.check_call(["btrfs", "subvolume", "snapshot", "-r",
-                               self.path, snapshot])
-
-    def delete(self):
-        subprocess.check_call(["btrfs", "subvolume", "delete", self.path])
-
-    def children_in(self, path):
-        if not os.path.isabs(path):
-            path = os.path.join(self.path, path)
-        children = []
-        for p in glob.glob(os.path.join(path, "*")):
-            try:
-                child = Subvolume.from_path(p)
-                if child.parent == self.uuid:
-                    children.append(child)
-            except subprocess.CalledProcessError:
-                pass
-        return children
-
-    @classmethod
-    def from_path(cls, path):
-        out = subprocess.check_output(["btrfs", "subvolume", "show", path],
-                                      stderr=subprocess.DEVNULL,
-                                      universal_newlines=True)
-        name = re.search("Name:\s+([^\n]+)", out).group(1)
-        uuid = re.search("uuid:\s+([^\n]+)", out).group(1)
-        parent = re.search("Parent uuid:\s+([^\n]+)", out).group(1)
-        if parent == "-":
-            parent = None
-        created = time.strptime(
-            re.search("Creation time:\s+([^\n]+)", out).group(1),
-            "%Y-%m-%d %H:%M:%S")
-        return cls(path, name, uuid, parent, created)
 
 
 def subvolume(s):
     try:
-        return Subvolume.from_path(s)
+        return btrfs.Subvolume.from_path(s)
     except subprocess.CalledProcessError:
         msg = "{} is not a subvolume".format(s)
         raise argparse.ArgumentTypeError(msg)
